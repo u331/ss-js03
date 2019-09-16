360bb82cb594759bcc1596ae1942b7b0b0e322db
u331 <3318740@ukr.net>
2019-09-16 12:58:52 +0300

    alpha2019-09-16_12-58-31

---
+++ ws.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.setHeader("Content-Type","text/html; charset=utf-8");
+    console.log(req.method);
+    res.end("Hi, <b>Jude</b>");
+}).listen(3030);
+
+http.get("http://127.0.0.1:3030", function (req, res) {
+    res.send('fc');
+})
+
+// http.get('/', function (req, res) {
+//         //res.end("hihi");
+//     }
+// );
